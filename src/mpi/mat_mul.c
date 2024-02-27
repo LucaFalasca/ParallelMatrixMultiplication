84c87088b9f50e1dd7d40c6a7cb70bb9000338c8
matteo <matteo.conti.977@gmail.com>
2024-02-27 19:25:41 +0100

    Inizio block cyclic dist

--- src/mpi/mat_mul.c
+++ src/mpi/mat_mul.c
@@ -3,15 +3,21 @@
 #include<string.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#include "util/utils.h"
 #define BLOCK_ROWS 2 
 #define BLOCK_COLS 2
 #define RSRC 0
 #define CSRC 0
+#define DATA_REP "native"
 
 
 void matrix_multiply(float *mat1, float *mat2, float *res, int r1, int c1, int c2);
 bool seq_check_result(float *mat1, float *mat2, float *res, int r1, int c1, int c2);
 void row_block_cyclic_distribution_old(float *mat, int row, int col, int block_size, int npc, MPI_Comm comm, int rank);
+void measure_submatrix_size_without_rem(int row, int col, int block_size, int pg_row, int pg_col);
+void measure_submatrix_size_with_rem(int row, int col, int block_size, int pg_row, int pg_col);
+float *block_cyclic_distribution(char *mat_path, int row, int col, int block_size, int pg_row, int pg_col, MPI_Comm comm, int rank, int size);
+
 //FOR 2d block cyclic distribution
 //Global array coordinates (i,j) so i*c+j is the linear index of the element
 //Global array element (i,j) destination process (Pr,Pc)=((RSRC+(i-1)/NR)%npr, (CSRC+(k-1)/NC)%npc)
@@ -25,75 +31,62 @@ void row_block_cyclic_distribution_old(float *mat, int row, int col, int block_s
 //Global column j local column index x in process P is x=(j-1)%NC
 
 int main(int argc, char *argv[]){
-    int rank, size,tag=0;
-    int r1, c1, r2, c2, npr, npc, block_size;
-    float *mat1, *mat2, *res;
-    char mat1path[128], mat2path[128];
+    int rank, size, tag=0;
+    int row_a, col_a, row_b, col_b, pg_row, pg_col, block_size;
+    float *mat_res;
+    char mat_a_path[128], mat_b_path[128];
     MPI_Comm comm;
     MPI_Init(&argc, &argv);
     MPI_Comm_dup(MPI_COMM_WORLD, &comm);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
     /*Get parameters from cmd*/
     if(argc<10){
         printf("Usage ./a.out <nrproc> <ncproc> <blocks> <mat1path> <rows1> <cols1> <mat2path> <rows2> <cols2> \n");
-        exit(1);
+        MPI_Abort(comm, 1);
     }
 
     //Process grid size
-    npr = atoi(argv[1]);
-    npc = atoi(argv[2]);
+    pg_row = atoi(argv[1]);
+    pg_col = atoi(argv[2]);
 
     /*Check size compatibility for process grid*/
-    if((npr*npc)!=size){
+    if((pg_row*pg_col)!=size){
         printf("Process grid size incompatible with number of processes spawned\n");
-        exit(1);
+        MPI_Abort(comm, 1);
     }
 
     //Block size
     block_size = atoi(argv[3]);
 
     //Matrix A data
-    strcpy(mat1path, argv[4]);
-    r1 = atoi(argv[5]);
-    c1 = atoi(argv[6]);
+    strcpy(mat_a_path, argv[4]);
+    row_a = atoi(argv[5]);
+    col_a = atoi(argv[6]);
 
     //Matrix B data
-    strcpy(mat2path, argv[7]);
-    r2 = atoi(argv[8]);
-    c2 = atoi(argv[9]);
+    strcpy(mat_b_path, argv[7]);
+    row_b = atoi(argv[8]);
+    col_b = atoi(argv[9]);
 
     /*Check size compatibility for matrix multiply*/
-    if(c1!=r2){
+    if(col_a!=row_b){
         printf("Incompatible matrix size for multiplication c1!=r2\n");
-        exit(1);
+        MPI_Abort(comm, 1);
     }
 
     #ifdef DEBUG
         if(rank==0){
             printf("Number of processes: %d\n", size);
-            printf("Process grid size: %d x %d\n", npr, npc);
+            printf("Process grid size: %d x %d\n", pg_row, pg_col);
             printf("Block size: %d x %d\n", block_size, block_size);
-            printf("Matrix A path %s size: %d x %d\n", argv[4], r1, c1);
-            printf("Matrix B path %s size: %d x %d\n", argv[6], r2, c2);
+            printf("Matrix A path %s size: %d x %d\n", mat_a_path, row_a, col_a);
+            printf("Matrix B path %s size: %d x %d\n", mat_b_path, row_b, col_b);
         }
     #endif
-
-    if(rank==0){
-        
-        res=(float *) malloc(r1*c2*sizeof(float));
-        if(res==NULL){
-            printf("Error in memory allocation for result matrix\n");
-            exit(1);
-        }
-        memset(res, 0, r1*c2*sizeof(float)); //TODO forse non lo metto
-        #ifdef DEBUG
-            printf("DEBUG\n");
-            printMatrix(mat1, r1, c1);
-        #endif
-    }
     
-    //row_block_cyclic_distribution(mat1, r1, c1, block_size, npc, comm, rank);
+    block_cyclic_distribution(mat_a_path, row_a, col_a, block_size, pg_row, pg_col, comm, rank, size);
     
     MPI_Finalize();
     return 0;
@@ -134,7 +127,45 @@ bool seq_check_result(float *mat1, float *mat2, float *res, int r1, int c1, int 
     return true;
 }
 
+//TODO
+float * block_cyclic_distribution(char *mat_path, int row, int col, int block_size, int pg_row, int pg_col, MPI_Comm comm, int rank, int size){
+    MPI_Status status;
+    MPI_Datatype mat_darray;
+    MPI_File mat_file;
+    int dims[2] = {row, col}; //Dimensione matrice originale
+    int distribs[2] = {MPI_DISTRIBUTE_CYCLIC, MPI_DISTRIBUTE_CYCLIC}; //Metodo di distribuzione dei blocchi
+    int dargs[2] = {block_size, block_size}; //Dimensione dei blocchi
+    int proc_dims[2] = {pg_row, pg_col}; //Dimensione della griglia di processi
+    float *recv_block;
 
+    /*
+    Creazione del tipo di dato per la matrice distribuita, ogni processo vedrà solo la sua porzione di matrice,
+    la porzione viene definita tramite block cyclic distribution
+    */
+    MPI_Type_create_darray(size, rank, 2, dims, distribs,dargs, proc_dims, MPI_ORDER_C, MPI_INT, &mat_darray);
+    MPI_Type_commit(&mat_darray);
+
+    //Apertura collettiva del file
+    MPI_File_open(comm, mat_path, MPI_MODE_RDONLY, MPI_INFO_NULL, &mat_file);
+    if (mat_file == MPI_FILE_NULL) {
+	    printf("File open error in block cyclic distribution\n");
+        MPI_Abort(comm, 1);
+    }
+
+    //Ogni processo ha una visione della matrice specificata dal darray creato in precedenza
+    MPI_File_set_view(mat_file, 2*sizeof(int), MPI_FLOAT, mat_darray, DATA_REP, MPI_INFO_NULL);
+    
+    MPI_File_read_all(mat_file, recv_block, sub_matrix_row * sub_matrix_col, MPI_FLOAT, &status);
+
+    MPI_File_close(&mat_file);
+
+}
+
+void measure_submatrix_size_without_rem(int row, int col, int block_size, int pg_row, int pg_col){
+    //TODO IMPLEMENTARLA E VEDERE SE POI LA READALL SI PUO USARE SPECIFICANDO LE TAGLIE PRECISE
+}
+
+void measure_submatrix_size_with_rem(int row, int col, int block_size, int pg_row, int pg_col){}
 
 //This function is used to distribute block of rows of a matrix over the process cyclically 
 void row_block_cyclic_distribution_old(float *mat, int row, int col, int block_size, int npc, MPI_Comm comm, int rank){
