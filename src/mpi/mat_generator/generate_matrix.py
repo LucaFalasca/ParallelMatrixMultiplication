d29c60c9fd8fec23b17297a4de662e49bfb47a51
Matteo Conti <matteo.conti.977@gmail.com>
2024-05-02 15:38:42 -0400

    Update collect data and generate matrix python scripts

--- src/mpi/mat_generator/generate_matrix.py
+++ src/mpi/mat_generator/generate_matrix.py
@@ -1,16 +1,22 @@
 import subprocess
 
+def execute_cmd(command):
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    # Print the output
+    print("Command Output:", result.stdout)
+
+    # Print the error, if any
+    if result.stderr:
+        print("Error:", result.stderr)
+    
+
 if __name__ == "__main__":
-    a=[32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 10000]
+    a = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 10000]
+    b = [32, 64, 128 ,156]
     
     for i in a:
-        for j in a:
-            if(i>=j):
-                command="make runt row1="+str(i)+" col1="+str(j) + " row2="+str(j)+" col2="+str(i)
-                result = subprocess.run(command, shell=True, capture_output=True, text=True)
-                # Print the output
-                print("Command Output:", result.stdout)
-
-                # Print the error, if any
-                if result.stderr:
-                    print("Error:", result.stderr)
+        command="make runt row1="+str(i)+" col1="+str(i) + " row2="+str(i)+" col2="+str(i)
+        execute_cmd(command)
+        for j in b:
+            command="make runt row1="+str(i)+" col1="+str(j) + " row2="+str(j)+" col2="+str(i)
+            execute_cmd(command)
