679f6c8e7e568e63cbfc1a9b5879bc5be7645e64
Matteo Conti <matteo.conti.977@gmail.com>
2024-05-10 05:43:30 -0400

    Results

--- src/mpi/collect_data.py
+++ src/mpi/collect_data.py
@@ -1,19 +1,9 @@
 import subprocess
 
-if __name__ == "__main__":
-    command = "mpirun -n {} ./a.out {} {} {} data/matrix/bin/mat1_{}x{}.bin {} {} data/matrix/bin/mat2_{}x{}.bin {} {} data/matrix/bin/mat3_{}x{}.bin data/matrix/bin/mat3_{}x{}_check.bin data/matrix/bin/result_{}.csv {}"
-    
-    n_proc = [1, 2, 4, 6, 8, 12, 16, 20]
-    pg_size = [[1, 1], [2, 1], [2, 2], [3, 2], [4, 2], [4, 3], [4, 4], [5, 4]]
-    block_size=32
-    
-    mat_nm_sizes = [50, 100, 500, 1000, 2048, 3000, 4096, 6000, 8192, 9000, 11000]
-    mat_k_sizes =[32, 64, 128, 156]
+def generate_sizes(mat_nm_sizes, mat_k_sizes):
     mat1_size = []
     mat2_size = []
     mat3_size = []
-    
-    
     for i in mat_nm_sizes:
         #Squared matrices
         mat1_size.append([i, i])
@@ -26,21 +16,37 @@ if __name__ == "__main__":
                 mat1_size.append([i, j])
                 mat2_size.append([j, i])
                 mat3_size.append([i, i])
-    
+    return mat1_size, mat2_size, mat3_size
+
+def run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size, blocked_string, blocked_flag):
+    command = "mpirun -n {} ./a.out {} {} {} data/matrix/bin/mat1_{}x{}.bin {} {} data/matrix/bin/mat2_{}x{}.bin {} {} data/matrix/bin/mat3_{}x{}.bin data/matrix/bin/mat3_{}x{}_check.bin data/matrix/bin/result_{}.csv {}"
     for i in range(0, len(n_proc)):
         for j in range(0, len(mat1_size)):
-            curr_cmd=command.format(n_proc[i], pg_size[i][0], pg_size[i][1], block_size, mat1_size[j][0], mat1_size[j][1], mat1_size[j][0], mat1_size[j][1], mat2_size[j][0], mat2_size[j][1], mat2_size[j][0], mat2_size[j][1], mat3_size[j][0], mat3_size[j][1], mat3_size[j][0], mat3_size[j][1], 'blocked', 1)
+            if not blocked_flag:
+                if (n_proc[i]==1 or n_proc[i]==2) and (mat1_size[j][0]==8192 and mat1_size[j][1]==8192):
+                    continue
+            curr_cmd=command.format(n_proc[i], pg_size[i][0], pg_size[i][1], block_size, mat1_size[j][0], mat1_size[j][1], mat1_size[j][0], mat1_size[j][1], mat2_size[j][0], mat2_size[j][1], mat2_size[j][0], mat2_size[j][1], mat3_size[j][0], mat3_size[j][1], mat3_size[j][0], mat3_size[j][1], blocked_string, blocked_flag)
             print("Launching ")
             print(curr_cmd)
             # Launch the C program using subprocess
-            #process = subprocess.Popen(curr_cmd, shell=True)
+            process = subprocess.Popen(curr_cmd, shell=True)
             # Wait for the process to finish
-            #process.wait()
-            curr_cmd=command.format(n_proc[i], pg_size[i][0], pg_size[i][1], block_size, mat1_size[j][0], mat1_size[j][1], mat1_size[j][0], mat1_size[j][1], mat2_size[j][0], mat2_size[j][1], mat2_size[j][0], mat2_size[j][1], mat3_size[j][0], mat3_size[j][1], mat3_size[j][0], mat3_size[j][1], 'non_blocked', 0)
-            print("Launching ")
-            print(curr_cmd)
-            # Launch the C program using subprocess
-            #process = subprocess.Popen(curr_cmd, shell=True)
-            # Wait for the process to finish
-            #process.wait()
+            process.wait()
+
+if __name__ == "__main__":
+    n_proc = [1, 2, 4, 6, 8, 12, 16, 20]
+    pg_size = [[1, 1], [2, 1], [2, 2], [3, 2], [4, 2], [4, 3], [4, 4], [5, 4]]
+    block_size=32
+    
+    mat_nm_sizes = [32, 64 ,128, 256, 400, 512, 800, 1024, 2048, 3000, 4096, 6000, 8192, 10000]
+    mat_k_sizes =[32, 64, 128, 156]
+    
+    mat1_size, mat2_size, mat3_size = generate_sizes(mat_nm_sizes, mat_k_sizes)
+    
+    #Unblocked matrix multiplication
+    run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size, 'non_blocked', 0)
+    
+    #Blocked matrix multiplication
+    run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size, 'blocked', 1)
+    
         
