6757f2a419c3b9ee2e9a67e49d69bc8050017258
Matteo Conti <matteo.conti.977@gmail.com>
2024-06-24 11:44:58 -0400

    risultati cuda_mpi

--- src/mpi/collect_data.py
+++ src/mpi/collect_data.py
@@ -22,10 +22,25 @@ def run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size,
     command = "mpirun -n {} ./a.out {} {} {} data/matrix/bin/mat1_{}x{}.bin {} {} data/matrix/bin/mat2_{}x{}.bin {} {} data/matrix/bin/mat3_{}x{}.bin data/matrix/bin/mat3_{}x{}_check.bin data/out/result_{}.csv {}"
     for i in range(0, len(n_proc)):
         for j in range(0, len(mat1_size)):
-            if not blocked_flag:
+            if blocked_flag==0:
                 if (n_proc[i]==1 or n_proc[i]==2) and (mat1_size[j][0]==8192 and mat1_size[j][1]==8192):
                     continue
             curr_cmd=command.format(n_proc[i], pg_size[i][0], pg_size[i][1], block_size, mat1_size[j][0], mat1_size[j][1], mat1_size[j][0], mat1_size[j][1], mat2_size[j][0], mat2_size[j][1], mat2_size[j][0], mat2_size[j][1], mat3_size[j][0], mat3_size[j][1], mat3_size[j][0], mat3_size[j][1], blocked_string, blocked_flag)
+            print("Launching ")
+            print(curr_cmd)
+            # Launch the C program using subprocess
+            process = subprocess.Popen(curr_cmd, shell=True)
+            # Wait for the process to finish
+            process.wait()
+
+def run_experiment_blocked(n_proc, pg_size, block_size, mat1_size, blocked_string, blocked_flag):
+    command = "mpirun -n {} ./a.out {} {} {} data/matrix/bin/mat1_{}x{}.bin {} {} data/matrix/bin/mat2_{}x{}.bin {} {} data/matrix/bin/mat3_{}x{}.bin data/matrix/bin/mat3_{}x{}_check.bin data/out/result_{}.csv {}"
+    for i in range(0, len(n_proc)):
+        for j in range(0, len(mat1_size)):
+            if blocked_flag==0:
+                if (n_proc[i]==1 or n_proc[i]==2) and (mat1_size[j][0]==8192 and mat1_size[j][1]==8192):
+                    continue
+            curr_cmd=command.format(n_proc[i], pg_size[i][0], pg_size[i][1], block_size, mat1_size[j][0], mat1_size[j][1], mat1_size[j][0], mat1_size[j][1], mat1_size[j][0], mat1_size[j][1], mat1_size[j][0], mat1_size[j][1], mat1_size[j][0], mat1_size[j][1], mat1_size[j][0], mat1_size[j][1], blocked_string, blocked_flag)
             print("Launching ")
             print(curr_cmd)
             # Launch the C program using subprocess
@@ -39,17 +54,20 @@ if __name__ == "__main__":
     block_size=32
     
     mat_nm_sizes = [32, 64 ,128, 256, 400, 512, 800, 1024, 2048, 3000, 4096, 6000, 8192, 10000]
+    mat_nm_sizes.reverse()
+    #mat_nm_sizes = [10000]
     mat_k_sizes =[32, 64, 128, 156]
     mat_k_sizes =[32, 64, 128, 156]
     
     mat1_size, mat2_size, mat3_size = generate_sizes(mat_nm_sizes, mat_k_sizes)
     
     #Unblocked matrix multiplication
-    #run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size, 'non_blocked', 0)
+    run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size, 'non_blocked', 0)
     
     #Blocked matrix multiplication
-    #run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size, 'blocked', 1)
+    run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size, 'blocked', 1)
 
-    run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size, 'accelerated', 2)
+    #run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size, 'accelerated', 2)
+    run_experiment(n_proc, pg_size, block_size, mat1_size, mat2_size, mat3_size, 'blocked', 2)
     
         
